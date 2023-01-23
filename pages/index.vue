49287d0d632eb8e6676f48d6856e281d985ee935
Felice <felicepng2010@gmail.com>
2023-01-23 17:13:00 +0800

    chore: update component names

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@ import { ref } from 'vue';
 
 const query = ref<Record<string, string>>({});
 
-const { data, pending } = await useFetch('/api/songRecs', {
+const { data, pending } = await useFetch('/api/songs', {
   query,
   server: false,
 });
@@ -25,14 +25,14 @@ defineExpose({
 <template>
   <div class="font-poppins flex flex-col">
     <NuxtLink to="/recents">&gt; to recents</NuxtLink>
-    <Form @search="onSearch" />
+    <Search @search="onSearch" />
 
     <div v-if="Object.keys(query).length === 0" />
     <div v-else-if="pending">Loading...</div>
     <div v-else-if="data?.error">Error: {{ data.error.message }}</div>
     <div v-else-if="data?.recs">
       <div v-if="data.recs.length === 0">No song recommendations found</div>
-      <Result v-for="rec in data.recs" :key="rec.id" v-bind="rec" />
+      <Song v-for="rec in data.recs" :key="rec.id" v-bind="rec" />
     </div>
     <div v-else>Error occurred retrieving song recommendations</div>
   </div>
